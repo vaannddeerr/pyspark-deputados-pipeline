f5a8391479b5c12c782005ade9edfd4646bc8efc
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-17 06:26:42 +0100

    :bug:Adicionando __init__.py na pasta src

--- src/pyspark_deputados/main.py
+++ src/pyspark_deputados/main.py
@@ -1,12 +1,6 @@
 from connectors.processing_api import DataPipeline
 from connectors.etl.etl_process import executar_etl
-import sys
-import os
 
-# Adiciona o diretório onde a main.py está ao path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
 
 menager = DataPipeline(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
 menager.save_file()
