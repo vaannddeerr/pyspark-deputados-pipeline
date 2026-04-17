c246d63fe0bc1a3f8b972a37e20f5284e37b82fa
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-17 06:35:21 +0100

    :bug:Adicionando __init__.py na pasta src

--- src/pyspark_deputados/main.py
+++ src/pyspark_deputados/main.py
@@ -2,6 +2,6 @@ from connectors.processing_api import DataPipeline
 from connectors.etl.etl_process import executar_etl
 
 
-menager = DataPipeline(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
+menager = DataPipeline(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome',is_path=False)
 menager.save_file()
 executar_etl(menager)
