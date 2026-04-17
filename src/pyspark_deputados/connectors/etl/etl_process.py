463546c265147db88a6559bd856cbcf97d942e8f
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-17 07:15:09 +0100

    :bug:Adicionando __init__.py na pasta src

--- src/pyspark_deputados/connectors/etl/etl_process.py
+++ src/pyspark_deputados/connectors/etl/etl_process.py
@@ -4,22 +4,19 @@ from pyspark.sql import functions as F
 
 def executar_etl(menager):
     path = '/Volumes/workspace/default/landing_zone/dadosabertos.json'
-    df = menager.read_df(path)
-
+    df = menager.read_df(path)\
+                .withColumn('dados', F.explode(F.col('dados')))\
+                .withColumn('id', F.col('dados.id'))\
+                .withColumn('email', F.col('dados.email'))\
+                .withColumn('idLegislatura', F.col('dados.idLegislatura'))\
+                .withColumn('nome', F.col('dados.nome'))\
+                .withColumn('siglaPartido', F.col('dados.siglaPartido'))\
+                .withColumn('siglaUf', F.col('dados.siglaUf'))\
+                .withColumn('uri', F.col('dados.uri'))\
+                .withColumn('uriPartido', F.col('dados.uriPartido'))\
+                .withColumn('urlFoto', F.col('dados.urlFoto')).drop(F.col('dados'))
     
-
-    df = df.withColumn('dados', F.explode(F.col('dados')))\
-           .withColumn('id', F.col('dados.id'))\
-           .withColumn('email', F.col('dados.email'))\
-           .withColumn('idLegislatura', F.col('dados.idLegislatura'))\
-           .withColumn('nome', F.col('dados.nome'))\
-           .withColumn('siglaPartido', F.col('dados.siglaPartido'))\
-           .withColumn('siglaUf', F.col('dados.siglaUf'))\
-           .withColumn('uri', F.col('dados.uri'))\
-           .withColumn('uriPartido', F.col('dados.uriPartido'))\
-           .withColumn('urlFoto', F.col('dados.urlFoto')).drop(F.col('dados'))
     
-    menager.write_df(df, 'overwrite')
-    return df
+    return menager.write_df(df)
     
 
