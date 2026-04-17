821fca856b80ce308a3e2faede9a7fa5e7c49185
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-17 04:17:21 +0100

    :bug:Adicionando __init__.py na pasta src

--- src/pyspark_deputados/main.py
+++ src/pyspark_deputados/main.py
@@ -1,11 +1,4 @@
-from pyspark_deputados.connectors.processing_api import DataPipeline
-
-import sys
-import os
-
-# Isso adiciona a pasta 'src' ao caminho de busca do Python
-sys.path.append(os.path.abspath('./src'))
-
+from connectors.processing_api import DataPipeline
 
 menager = DataPipeline(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
 menager.save_file()
