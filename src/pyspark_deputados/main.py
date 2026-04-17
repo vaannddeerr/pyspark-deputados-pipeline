b462dd2cd0b39c87eed91bc9eea09354b49515a9
Vanderlei Sousa de Carvalho <wsc_2014@live.com>
2026-04-17 03:42:42 +0100

    :bug:Correção de bug

--- src/pyspark_deputados/main.py
+++ src/pyspark_deputados/main.py
@@ -3,10 +3,8 @@ from pyspark_deputados.connectors.processing_api import DataPipeline
 import sys
 import os
 
-# Pega o caminho do diretório onde o main.py está localizado
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Adiciona esse caminho ao sistema para que ele enxergue as pastas vizinhas
-sys.path.append(current_dir)
+# Isso adiciona a pasta 'src' ao caminho de busca do Python
+sys.path.append(os.path.abspath('./src'))
 
 
 menager = DataPipeline(url='https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome')
